4b87974bbd16f6f8c352197514d47c3d2a24add1
june <llovejune@nate.com>
2022-01-10 09:21:09 -0500

    commit

--- Algorithm_coding_test/Programmers/hash(1).py
+++ Algorithm_coding_test/Programmers/hash(1).py
@@ -1,63 +1,62 @@
 # 완주하지 못한 마라톤 선수
-# from typing import Counter
+import collections
+from typing import Counter
 
 
-# def solution(participant, completion):
-#     extract = []
-#     duplicate = Counter(participant)
+def solution(participant, completion):
+    extract = []
+    duplicate = Counter(participant)
 
-#     for i in range(len(participant)):
-#         if participant[i] not in completion:
-#             extract.append(participant[i])
+    for i in range(len(participant)):
+        if participant[i] not in completion:
+            extract.append(participant[i])
 
-#     if len(extract) == 0:
-#         for key, value in duplicate.items():
-#             if value > 1:
-#                 extract.append(key)
+    if len(extract) == 0:
+        for key, value in duplicate.items():
+            if value > 1:
+                extract.append(key)
 
-#     answer = "".join(extract)
-#     return answer
+    answer = "".join(extract)
+    return answer
 
 
-# participant = ["mislav", "stanko", "mislav", "ana"]
-# completion = ["stanko", "ana", "mislav"]
-# print(solution(participant, completion))
+participant = ["mislav", "stanko", "mislav", "ana"]
+completion = ["stanko", "ana", "mislav"]
+print(solution(participant, completion))
 
 ### 위에 시간 초과 ####
 
 
-# def solution(participant, completion):
-#     pass_list = []  # 완주한 인간들
-#     return_list = []  # 완주 못한 인간들
-#     extract = []  # 중복된 인간들
+def solution(participant, completion):
+    pass_list = []  # 완주한 인간들
+    return_list = []  # 완주 못한 인간들
+    extract = []  # 중복된 인간들
 
-#     for i in range(len(participant)):
-#         if participant[i] not in completion and participant[i] not in pass_list:
-#             return_list.append(participant[i])  # 완주 못한 인간들
-#         elif participant[i] in completion and participant[i] not in pass_list:
-#             pass_list.append(participant[i])  # 완주한 인간들
-#         elif participant[i] in pass_list and participant[i] in pass_list:
-#             extract.append(participant[i])  # 중복된 인간들
+    for i in range(len(participant)):
+        if participant[i] not in completion and participant[i] not in pass_list:
+            return_list.append(participant[i])  # 완주 못한 인간들
+        elif participant[i] in completion and participant[i] not in pass_list:
+            pass_list.append(participant[i])  # 완주한 인간들
+        elif participant[i] in pass_list and participant[i] in pass_list:
+            extract.append(participant[i])  # 중복된 인간들
 
-#     print('완주한 인간들', pass_list)
-#     print('완주 못한 인간들', return_list)
-#     print('중복된 인간들', extract)
+    print('완주한 인간들', pass_list)
+    print('완주 못한 인간들', return_list)
+    print('중복된 인간들', extract)
 
-#     if len(return_list) == 0:
-#         answer = "".join(extract)
-#     else:
-#         answer = "".join(return_list)
+    if len(return_list) == 0:
+        answer = "".join(extract)
+    else:
+        answer = "".join(return_list)
 
-#     return answer
+    return answer
 
 
-# participant = ["marina", "josipa", "nikola", "vinko", "filipa"]
-# completion = ["josipa", "filipa", "marina", "nikola"]
-# print(solution(participant, completion))
+participant = ["marina", "josipa", "nikola", "vinko", "filipa"]
+completion = ["josipa", "filipa", "marina", "nikola"]
+print(solution(participant, completion))
 
 # 시간초과 2번 맞고, O(n2) 프로그래머스 풀이 봤음. 이거 우쩨 이렇게 풀었노,;;;
-
-import collections
 
 
 def solution(participant, completion):
